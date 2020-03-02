f9de7c89ef006eceb3e74aa245f224ed3753546e
Aqeel <aqeelaqeelzeid@gmail.com>
2020-03-02 09:23:41 +0530

    Added The BackEnd Spring Skeleton
    Skeleton App with all the dependencies

---
+++ zendayas-backEnd/zendayaBackend/src/main/java/com/coconutcoders/zendaya/zendayaBackend/ZendayaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coconutcoders.zendaya.zendayaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZendayaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZendayaBackendApplication.class, args);
+	}
+
+}
---
+++ zendayas-backEnd/zendayaBackend/src/test/java/com/coconutcoders/zendaya/zendayaBackend/ZendayaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coconutcoders.zendaya.zendayaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZendayaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
