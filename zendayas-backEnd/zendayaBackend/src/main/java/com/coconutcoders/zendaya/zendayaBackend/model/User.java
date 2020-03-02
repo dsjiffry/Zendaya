a2d0ba6e3849c0a125ccc284e3f2b29461f5961a
Aqeel <aqeelaqeelzeid@gmail.com>
2020-03-02 14:56:47 +0530

    Implemented Basic UserAuthentication with static user
    username = foo and password = foo

---
+++ zendayas-backEnd/zendayaBackend/src/main/java/com/coconutcoders/zendaya/zendayaBackend/model/User.java
@@ -0,0 +1,20 @@
+package com.coconutcoders.zendaya.zendayaBackend.model;
+
+public class User {
+
+    String userName;
+    String email;
+    String password;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+
+
+}
