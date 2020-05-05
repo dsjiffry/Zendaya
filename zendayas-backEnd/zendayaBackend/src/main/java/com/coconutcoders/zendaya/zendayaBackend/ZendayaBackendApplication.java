7ab944ac9c0e9ffbe98508079401c590f045de66
Shehan Jiffry <dsjiffry@gmail.com>
2020-05-05 08:15:23 +0530

    moved database to mongodb Atlas
    Signed-off-by: Shehan Jiffry <dsjiffry@gmail.com>

--- zendayas-backEnd/zendayaBackend/src/main/java/com/coconutcoders/zendaya/zendayaBackend/ZendayaBackendApplication.java
+++ zendayas-backEnd/zendayaBackend/src/main/java/com/coconutcoders/zendaya/zendayaBackend/ZendayaBackendApplication.java
@@ -3,10 +3,23 @@ package com.coconutcoders.zendaya.zendayaBackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 @SpringBootApplication
 public class ZendayaBackendApplication {
 
 	public static void main(String[] args) {
+
+		//Getting the IP address
+		try {
+			URL url = new URL("http://checkip.amazonaws.com/");
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			System.out.println("------------------------> " + br.readLine() +" : "+ System.getenv("$PORT"));
+		} catch (Exception ignored) {}
+
+
 		SpringApplication.run(ZendayaBackendApplication.class, args);
 	}
 
