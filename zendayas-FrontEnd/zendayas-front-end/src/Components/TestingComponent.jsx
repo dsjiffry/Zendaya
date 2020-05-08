cbffcf119d531800b199d6b9201ecdb4639ffb36
Aqeel-Zeid <aqeelaqeelzeid@gmail.com>
2020-05-08 18:01:15 +0530

    Added Test Components to test router and context

---
+++ zendayas-FrontEnd/zendayas-front-end/src/Components/TestingComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TestingComponent() {
+    return (
+        <div>
+            This is a testing Component
+        </div>
+    )
+}
---
+++ zendayas-FrontEnd/zendayas-front-end/src/Pages/TestPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TestingComponent from "../Components/TestingComponent"
+
+export default function TestPage() {
+    return (
+        <div>
+            This Is A test Page Created For testing purpose
+            <TestingComponent/>
+        </div>
+    )
+}
