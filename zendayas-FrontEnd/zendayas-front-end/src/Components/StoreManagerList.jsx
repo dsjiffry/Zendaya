120fb387cd6866c066934a6852777710b1062931
Aqeel-Zeid <aqeelaqeelzeid@gmail.com>
2020-05-13 22:38:44 +0530

    Store manager Delete and Edit Complete

--- zendayas-FrontEnd/zendayas-front-end/src/Components/StoreManagerList.jsx
+++ zendayas-FrontEnd/zendayas-front-end/src/Components/StoreManagerList.jsx
@@ -2,11 +2,105 @@ import React from 'react'
 
 import {  Segment as Section, Button, Table} from 'semantic-ui-react'
 
+import userManagement from "../Contexts/Helper_Functions/user_management"
+import StoreManagerRow from './StoreManagerRow'
+
+
+
 export default function StoreManagerList() {
+
+    const [state, setstate] = React.useState({storeManagers:[] })
+
+    const [elements , setElements] = React.useState([])
+
+    let rows = [];
+
+
+    async function fetchStoreManagers()
+        {
+            let command_GET_ALL_STORE_MANAGER = {
+                type : "GET_ALL_STORE_MANAGER",
+                payload : { GASM_search_keyword : "" }
+            }
+
+            let result_GET_ALL_STORE_MANAGER = await userManagement(command_GET_ALL_STORE_MANAGER);
+
+            console.log(result_GET_ALL_STORE_MANAGER);
+
+            if(result_GET_ALL_STORE_MANAGER.status === 200)
+            {
+                setstate({storeManagers : result_GET_ALL_STORE_MANAGER.payload.data})
+                
+                Object.keys(state.storeManagers).forEach(function(key,index) {
+            
+                    rows.push(
+                        <StoreManagerRow
+                            username = {state.storeManagers[key].username}
+                            password = {state.storeManagers[key].password}
+                            email = {state.storeManagers[key].email}
+                            key = {state.storeManagers[key].username}
+                        />
+                    )
+        
+                });
+
+                console.log(rows)
+                 
+            }
+            else
+            {
+                result_GET_ALL_STORE_MANAGER.status === 404 ? alert("Store Mangers Not Found") : alert("Server Error")
+            }
+           
+
+        }
+
+
+    React.useEffect(() => {
+        
+        fetchStoreManagers()
+      
+        return () => {
+            console.log("Clean Up Code useEffect StoreManagerList()")
+        }
+    }, [])
+
+
+    React.useEffect(() => {
+        
+        Object.keys(state.storeManagers).forEach(function(key,index) {
+            
+            rows.push(
+                <StoreManagerRow
+                    username = {state.storeManagers[key].username}
+                    password = {state.storeManagers[key].password}
+                    email = {state.storeManagers[key].email}
+                    key = {state.storeManagers[key].username}
+                />
+            )
+
+        });
+        
+        console.log(rows)
+    
+        setElements(rows)
+
+        return () => {
+            
+        }
+    }, [state])
+
+
+
     return (
         <Table inverted>
             <Table.Header fullWidth>
-                <Table.HeaderCell colSpan='3'> Store Manager List </Table.HeaderCell>
+                <Table.Row>
+                    <Table.HeaderCell colSpan='2'> Store Manager List </Table.HeaderCell>
+                    <Table.HeaderCell colSpan='2'> 
+                        <Button primary onClick = {() => {fetchStoreManagers()}}>Refresh List</Button> 
+                    </Table.HeaderCell>
+                </Table.Row>
             </Table.Header>
             <Table.Header>
                 <Table.Row>
@@ -17,19 +111,9 @@ export default function StoreManagerList() {
             </Table.Header>
 
             <Table.Body>
-                <Table.Row>
-                    <Table.Cell>Jhon Doe</Table.Cell>
-                    <Table.Cell>Jhon@gmail.com</Table.Cell>
-                    <Table.Cell>
-
-                        <Button.Group fluid>
-                            <Button negative>Delete</Button>
-                            <Button.Or />
-                            <Button positive>Edit</Button>
-                        </Button.Group>
-                    </Table.Cell>
-                </Table.Row>
-
+                {
+                    elements
+                }
             </Table.Body>
         </Table>
     )
